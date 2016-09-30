0bfa9bf6047424ac4f121edbd41b9553be4c18d0
William Wolf <wwolf@shopkeep.com>
2016-09-30 14:51:27 +0100

    employ softmax_cross_entropy_with_logits for loss instead of hand-rolled cross-entropy loss

--- linear_model.py
+++ linear_model.py
@@ -4,19 +4,19 @@ from willwolf.tensorflow_helpers.model import TensorFlowBaseModel, graph_node
 
 
 class LogisticRegression(TensorFlowBaseModel):
-    
+
     def __init__(self, dataset, learning_rate, beta=None):
         self.dataset = dataset
         self._learning_rate = learning_rate
         self._beta = beta
         super().__init__()
-        
+
     @property
     def _graph_nodes(self):
-        return [self.feed_forward, self.optimize, self.compute_loss, self.compute_accuracy]
-    
+        return [self._compute_logits, self.optimize, self.compute_loss, self.compute_accuracy]
+
     @graph_node
-    def feed_forward(self):
+    def _compute_logits(self):
         num_inputs = int(self.dataset.data.get_shape()[1])
         num_labels = int(self.dataset.labels.get_shape()[1])
         self._weights = tf.Variable(
@@ -27,26 +27,30 @@ class LogisticRegression(TensorFlowBaseModel):
             initial_value=tf.zeros(shape=(num_labels,)),
             name='biases'
         )
-        logits = tf.matmul(self.dataset.data, self._weights) + self._biases
+        return tf.matmul(self.dataset.data, self._weights) + self._biases
+
+    @graph_node
+    def _compute_predictions(self):
+        logits = self._compute_logits()
         return tf.nn.softmax(logits)
-        
+
     @graph_node
     def optimize(self):
         loss = self.compute_loss()
         return tf.train.GradientDescentOptimizer(learning_rate=self._learning_rate).minimize(loss)
-    
+
     @graph_node
     def compute_loss(self):
-        predictions = self.feed_forward()
-        loss = -tf.reduce_sum(self.dataset.labels * tf.log(predictions))
+        logits = self._compute_logits()
+        loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=self.dataset.labels)
         if self._beta:
             loss += self._beta * tf.nn.l2_loss(self._weights) + \
                     self._beta * tf.nn.l2_loss(self._biases)
         return tf.reduce_mean(loss)
-    
+
     @graph_node
     def compute_accuracy(self):
-        predictions = self.feed_forward()
+        predictions = self._compute_predictions()
         mistakes = tf.not_equal(
             tf.argmax(self.dataset.labels, 1), tf.argmax(predictions, 1)
         )
